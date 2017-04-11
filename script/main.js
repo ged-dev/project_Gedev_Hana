edacb5273feda4e9a464da360d5c2322d1a211ed
Gerald Devreux <gerald.devreux.pro@gmail.com>
2017-04-11 16:03:06 +0200

    Deplacement du personnage gauche/droite : modif du code

--- script/main.js
+++ script/main.js
@@ -13,46 +13,58 @@ function create(){
 	game.stage.backgroundColor = "#77A6B6";
 	spriteCharacter = game.add.sprite(300, 200, 'character');
 
-	let walkLeft = spriteCharacter.animations.add('walkLeft', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
-	let walkRight = spriteCharacter.animations.add('walkRight', [9, 10, 11, 12, 13, 14, 15, 16, 17], 10, true);
+	let walkLeft = spriteCharacter.animations.add('walkLeft', [0, 1, 2, 3, 4, 5, 6, 7, 8], 15, true);
+	let walkRight = spriteCharacter.animations.add('walkRight', [9, 10, 11, 12, 13, 14, 15, 16, 17], 15, true);
 
 	spriteCharacter.frame = 14;
 	// SCAALE
 
-	//spriteCharacter.scale.setTo(rand, rand);
-	game.physics.arcade.enable( spriteCharacter );
-	spriteCharacter.body.velocity.x = 0;
+	// --------- INIT PHYSICS ----------- \\
+	game.physics.arcade.enable( spriteCharacter, Phaser.Physics.ARCADE );
+
+	// Velocity - Gravity
+	game.physics.arcade.gravity.y = 500;
 	
 	cursors = this.input.keyboard.createCursorKeys();
 	jump = this.input.keyboard.addKey( Phaser.KeyCode.SPACEBAR);
-	spriteCharacter.body.maxVelocity.set(10);
+	spriteCharacter.body.maxVelocity.set(150);
 
+	leftButton = this.input.keyboard.addKey( Phaser.KeyCode.Q);
+	downButton = this.input.keyboard.addKey( Phaser.KeyCode.S);
+	upButton = this.input.keyboard.addKey( Phaser.KeyCode.Z);
+	rightButton = this.input.keyboard.addKey( Phaser.KeyCode.D);
 };
 
 function update(){
+	spriteCharacter.body.velocity.x = 0;
 
-	if (jump.isDown){
-		spriteCharacter.body.velocity.y = -100;
-		console.log(spriteCharacter.body.velocity.y); 
-	}
+	if (cursors.left.isDown)
+    {
+        //  Move to the left
+        spriteCharacter.body.velocity.x = -200;
 
+        spriteCharacter.animations.play('walkLeft');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        spriteCharacter.body.velocity.x = 150;
 
-	// RIGHT
-	if ( cursors.right.isDown){
-		spriteCharacter.body.x += 5;
-		spriteCharacter.animations.play('walkRight', 16, true);
-	}else if (cursors.right.isUp && cursors.left.isUp){
-	 	spriteCharacter.animations.stop(null, true);
-	}
-	
-	
-	// LEFT
-	if ( cursors.left.isDown){
-		spriteCharacter.body.x -= 5;
-		spriteCharacter.animations.play('walkLeft', 16, true);
-	} else if (cursors.left.isUp && cursors.right.isUp){
-		spriteCharacter.animations.stop(null, true);
-	}
+        spriteCharacter.animations.play('walkRight');
+    }
+    else
+    {
+        //  Stand still
+        spriteCharacter.animations.stop();
+        //spriteCharacter.frame = 4;
+    }
+
+    if (jump.isDown)
+    {
+        spriteCharacter.body.velocity.y = -250;
+    }else if (jump.isUp){
+    	spriteCharacter.body.velocity.y = 0;
+    }
 };
 
 
