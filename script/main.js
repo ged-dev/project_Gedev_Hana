4209159f7c6f1b9c052e09ea7217e85b298db21f
BastienDeclaye <bastiendeclayepro@gmail.com>
2017-04-12 15:43:52 +0200

    gravity correction

--- script/main.js
+++ script/main.js
@@ -1,31 +1,28 @@
-var game = new Phaser.Game(1200, 768, Phaser.AUTO, 'game',
+let game = new Phaser.Game(1200, 768, Phaser.AUTO, 'game',
         {preload: preload, create: create, update: update, render: render});
 
-var spriteCharacter;
-var map;
-var score = 0;
-var death = 0;
-var jumpTimer = 0;
-var gravityTimer = 0;
-
-var compteurTest = 0;
+let spriteCharacter, map, score = 0, death = 0, layer, blocBleu, timerSpikes = 0, timerJump = 0;
 
 
 function hitCoin(sprite, tile) {
-
     map.removeTile(tile.x, tile.y);
     score++;
     console.log(score);
     return false;
-}
-
+};
+function hitSpike(sprite, tile) {
+    if (game.time.now > timerSpikes){
+    spriteCharacter.damage(1);
+    console.log(spriteCharacter.health);
+    timerSpikes = game.time.now + 1000;
+    }
+};
 
 function preload() {
     game.load.spritesheet('character', 'assets/graphics/sprite_player.png', 30, 49, 18);
     game.load.image('tiles', 'assets/graphics/sheet1.png');
     game.load.tilemap('gravity', 'assets/levels/gravity.json', null, Phaser.Tilemap.TILED_JSON);
-}
-;
+};
 
 
 function create() {
@@ -33,17 +30,22 @@ function create() {
     map.addTilesetImage('sheet1', 'tiles');
     layer = map.createLayer('Fond');
     blocBleu = map.createLayer('Gravity');
-
+    spikes = map.createLayer('Spikes');
     console.log(layer);
+    console.log(blocBleu);
+
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
-    console.log(map);
+    blocBleu.resizeWorld();
+    map.setCollisionBetween(1, 15, true, blocBleu);
+    map.setCollisionBetween(1, 15, true, spikes);
     map.setCollisionBetween(1, 15);
     map.setTileIndexCallback(10, hitCoin, this);
-
+    map.setTileIndexCallback([3,15], hitSpike, this, spikes);
 
     game.stage.backgroundColor = "#77A6B6";
     spriteCharacter = game.add.sprite(50, 50, 'character');
+
 
     // Create ANIMATIONS
     let walkLeft = spriteCharacter.animations.add('walkLeft', [0, 1, 2, 3, 4, 5, 6, 7, 8], 15, true);
@@ -51,6 +53,7 @@ function create() {
 
     spriteCharacter.frame = 14;
     spriteCharacter.inverted = false;
+    spriteCharacter.health = 3;
 
 
     // --------- INIT PHYSICS ----------- \\
@@ -72,8 +75,8 @@ function create() {
     downButton = this.input.keyboard.addKey(Phaser.KeyCode.S);
     upButton = this.input.keyboard.addKey(Phaser.KeyCode.Z);
     rightButton = this.input.keyboard.addKey(Phaser.KeyCode.D);
-}
-;
+};
+
 
 function callback(sprite, tile) {
     // if(tile.index == 14){
@@ -83,27 +86,29 @@ function callback(sprite, tile) {
     if (!spriteCharacter.inverted && spriteCharacter.body.blocked.up) {
         game.physics.arcade.gravity.y *= -1;
 
-        console.log(game.physics.arcade.gravity.y);
 
-        spriteCharacter.angle = 180;
+        spriteCharacter.scale.y *=-1;
         spriteCharacter.inverted = true;
     } else if (spriteCharacter.inverted && spriteCharacter.body.blocked.down){
         game.physics.arcade.gravity.y *= -1;
         
-        spriteCharacter.angle = 0;
+        spriteCharacter.scale.y *=-1;
         spriteCharacter.inverted = false;
     }
+};
+
+function banane(sprite, tile){
+    console.log("coucou");
 }
 
 function update() {
     game.physics.arcade.collide(spriteCharacter, layer);
-
-    game.physics.arcade.collide(spriteCharacter, blocBleu, callback());
+    if(spriteCharacter.body.blocked.up){console.log("blocked")};
+    game.physics.arcade.collide(spriteCharacter, blocBleu, callback);
+    game.physics.arcade.collide(spriteCharacter, spikes);
 
 
     spriteCharacter.body.velocity.x = 0;
-
-
 
 
     if (cursors.left.isDown || leftButton.isDown)
@@ -129,14 +134,14 @@ function update() {
     {
         if (spriteCharacter.inverted && spriteCharacter.body.blocked.up){
             spriteCharacter.body.velocity.y = 285;
+            timerJump = game.time.now + 500;
         } else if(spriteCharacter.body.blocked.down) {
             spriteCharacter.body.velocity.y = -285;
-        } 
+            timerJump = game.time.now + 500;
+        }
+        
     }
-}
-;
-
-
+};
 
 
 function render() {}
